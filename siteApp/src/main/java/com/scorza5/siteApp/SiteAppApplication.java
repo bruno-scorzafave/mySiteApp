0c105c5cda22a179447edf53aad9e2e7c328500f
Bruno Scorzafave <bruno.scorzafave@aluno.ufabc.edu.br>
2021-07-20 22:41:24 -0300

    Projeto de Spring boot com implementação de um controller e uma view para teste

---
+++ siteApp/src/main/java/com/scorza5/siteApp/SiteAppApplication.java
@@ -0,0 +1,13 @@
+package com.scorza5.siteApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiteAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiteAppApplication.class, args);
+	}
+
+}
---
+++ siteApp/src/test/java/com/scorza5/siteApp/SiteAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scorza5.siteApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiteAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
