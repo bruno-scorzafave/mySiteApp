b4faa286bdd41b0f21b1d21060257859d68c643c
scorza5 <bruno.scorzafave@aluno.ufabc.edu.br>
2021-07-22 12:41:21 -0300

    Tried to implement database configuration, the table is never created

--- siteApp/src/main/java/com/scorza5/models/Visita.java
+++ siteApp/src/main/java/com/scorza5/models/Visita.java
@@ -1,8 +1,23 @@
 package com.scorza5.models;
 
+import java.io.Serializable;
 import java.util.Date;
 
-public class Visita {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="visitas")
+public class Visita implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy= GenerationType.AUTO)
+	private long id;
 	
 	public String nome;
 	public String empresa;
@@ -10,6 +25,12 @@ public class Visita {
 	public String email;
 	
 	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
 	public String getNome() {
 		return nome;
 	}
